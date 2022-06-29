8fdaf4f947c743e2aebd37ff93e59b95b2c8598b
Anchit Pokharel <pokharelanchit@gmail.com>
2022-06-29 11:43:16 +0545

    Created an app. Made 2 models, magrated them.

--- formsandblog/models.py
+++ formsandblog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class send_message(models.Model):
+    
+    your_name = models.CharField(max_length=255)
+    your_number = models.CharField(max_length=255)
+    your_email = models.EmailField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField(max_length=255)
+    
+class contact_us(models.Model):
+    
+    your_name = models.CharField(max_length=255)
+    your_number = models.CharField(max_length=255)
+    your_email = models.EmailField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField(max_length=255)
+    
