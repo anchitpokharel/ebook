a07d94a04b076cf3e583c12c80aeedad9b3e65e1
Anchit Pokharel <pokharelanchit@gmail.com>
2022-07-05 12:17:17 +0545

    changed the model and migrated again

--- formsandblog/models.py
+++ formsandblog/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
-# Create your models here.
-class send_message(models.Model):
-    
-    your_name = models.CharField(max_length=255)
-    your_number = models.CharField(max_length=255)
-    your_email = models.EmailField(max_length=255)
-    subject = models.CharField(max_length=255)
-    message = models.TextField(max_length=255)
-    
+# Create your models here.    
 class contact_us(models.Model):
     
     your_name = models.CharField(max_length=255)
