3cc7e03729c6b650c03da239e4115c66e7f30eb8
Anchit Pokharel <pokharelanchit@gmail.com>
2022-07-05 12:02:33 +0545

    Templating donegit status

--- formsandblog/urls.py
+++ formsandblog/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index)
+    path('index/', views.index),
+    path('accountspayable/', views.accounts_payable),
+    path('invoice/', views.invoice),
+    path('payroll/', views.payroll),
+    path('reconciliation/', views.reconciliation),
+    path('reporting/', views.reporting),
+    path('setup_training/', views.setup_training),
+    path('virtual_bookkeeping/', views.virtual_bookkeeping),
 ]
--- formsandblog/views.py
+++ formsandblog/views.py
@@ -2,4 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    bootstrap_class = "service-homebtn"
+    return render(request, 'index.html', {bootstrap_class: bootstrap_class})
+
+def accounts_payable(request):
+    return render(request, 'accounts_payable.html')
+
+def invoice(request):
+    return render(request, 'invoice.html')
+
+def payroll(request):
+    return render(request, 'payroll.html')
+
+def reconciliation(request):
+    return render(request, 'reconciliation.html')
+
+def reporting(request):
+    return render(request, 'reporting.html')
+
+def setup_training(request):
+    return render(request, 'setup-training.html')
+
+def virtual_bookkeeping(request):
+    return render(request, 'virtual-bookkeeping.html')
